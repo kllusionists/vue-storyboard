d4ccf7d25c8d5a0c28450811786cf40f7c2cb96b
ChanningKuo <492689363@qq.com>
2021-03-03 13:26:24 +0800

    gzip not delete original files

--- vue.config.js
+++ vue.config.js
@@ -97,7 +97,7 @@ module.exports = {
             threshold: 10240, // 只有大小大于该值的资源会被处理 10240
             test: new RegExp('\\.(' + ['js'].join('|') + ')$'),
             minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
-            deleteOriginalAssets: true // 删除原文件
+            deleteOriginalAssets: false // 删除原文件
           }
         )
       )
